8359ce73ea04c4721c4e61463fb36cf28170fcd9
nmosier <nmosier@middlebury.edu>
2019-09-29 13:08:02 +0000

    final commit before this is trashed

--- src/middfs.c
+++ src/middfs.c
@@ -3,33 +3,90 @@
 #include <sys/module.h>
 #include <sys/systm.h>
 #include <sys/mount.h>
-#include <fs/pseudofs/pseudofs.h>
 #include <sys/sbuf.h>
 
-int middfs_init(struct pfs_info *pi, struct vfsconf *vfs);
-int middfs_uninit(struct pfs_info *pi, struct vfsconf *vfs);
-int middfs_file(struct thread *td, struct proc *p, struct pfs_node *pn,
-		struct sbuf* sb, struct uio *uio);
+#define FSNAME "middfs"
 
 
-int middfs_file(struct thread *td, struct proc *p, struct pfs_node *pn,
-		struct sbuf* sb, struct uio *uio) {
-  const char *contents = "this is a synthetic file\n";
-  sbuf_bcpy(sb, contents, strlen(contents));
+int middfs_mount(struct mount *mp);
+int middfs_unmount(struct mount *mp, int mntflags);
+int middfs_eventhandler(struct module *mp, int event, void *data);
+int middfs_vfsop_root(struct mount *mp, int lkflags,
+		      struct vnode **vpp);
+int middfs_vfsop_statfs(struct mount *mp, struct statfs *sbp);
+
+static struct vfsops middfs_vfsops =
+  {.vfs_mount = middfs_mount,
+   .vfs_unmount = middfs_unmount,
+   .vfs_statfs = middfs_vfsop_statfs,
+   .vfs_root = middfs_vfsop_root,
+  };
+
+static struct vfsconf middfs_vfsconf =
+  {.vfc_version = VFS_VERSION,
+   .vfc_name = FSNAME,
+   .vfc_vfsops = &middfs_vfsops,
+   .vfc_typenum = -1,
+   .vfc_flags = VFCF_JAIL | VFCF_SYNTHETIC
+  };
+
+	      
+
+static struct moduledata middfs_moddata =
+  {.name = FSNAME,
+   .evhand = middfs_eventhandler,
+   .priv = &middfs_vfsconf
+  };
+
+
+int middfs_mount(struct mount *mp) {
+  printf("middfs: mounted\n");
   return 0;
 }
 
-int middfs_init(struct pfs_info *pi, struct vfsconf *vfs) {
-  struct pfs_node *root = pi->pi_root; /* mount point */
-  
-  uprintf("middfs initialized\n");
-  pfs_create_file(root, "file", middfs_file, NULL, NULL, NULL, PFS_RD);
-  return 0;
-}
-  
-int middfs_uninit(struct pfs_info *pi, struct vfsconf *vfs) {
-  uprintf("middfs uninitialized\n");  
+int middfs_unmount(struct mount *mp, int mntflags) {
+  printf("middfs: unmounted\n");
   return 0;
 }
 
-PSEUDOFS(middfs, 1, VFCF_JAIL);
+int middfs_eventhandler(struct module *mp, int event, void *data) {
+  int err = 0;
+
+  switch (event) {
+  case MOD_LOAD:
+    if ((err = vfs_modevent(NULL, event, &middfs_vfsconf))) {
+      uprintf("middfs loading error\n");
+      return err;
+    }
+    uprintf("middfs loaded\n");
+    break;
+  case MOD_UNLOAD:
+    if ((err = vfs_modevent(NULL, event, &middfs_vfsconf))) {
+      uprintf("middfs unloading error\n");
+      return err;
+    }
+    uprintf("middfs unloaded\n");
+    break;
+  default:
+    uprintf("middfs: unsupported event\n");
+    err = EOPNOTSUPP;
+    break;
+  }
+
+  return err;
+}
+
+int middfs_vfsop_root(struct mount *mp, int lkflags,
+		      struct vnode **vpp) {
+  uprintf("middfs root\n");
+  return 0;
+}
+
+int middfs_vfsop_statfs(struct mount *mp, struct statfs *sbp) {
+  uprintf("middfs statfs\n");
+  return 0;
+}
+
+
+DECLARE_MODULE(middfs, middfs_moddata, SI_SUB_VFS, SI_ORDER_MIDDLE);
+MODULE_VERSION(middfs, 1);
