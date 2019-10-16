ae60c1d1d922f1ab836f4b3a74dc38bd68f7d45c
nmosier <nmosier@middlebury.edu>
2019-10-16 17:37:05 +0000

    added server_loop() which polls

--- src/server/middfs-server-conn.h
+++ src/server/middfs-server-conn.h
@@ -5,7 +5,10 @@
 #ifndef __MIDDFS_SERVER_CONN_H
 #define __MIDDFS_SERVER_CONN_H
 
+#include "middfs-sock.h"
+
 int server_start(const char *port, int backlog);
 int server_accept(int servfd);
+int server_loop(struct middfs_socks *socks);
 
 #endif
