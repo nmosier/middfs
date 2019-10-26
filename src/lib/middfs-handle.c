d4b8c096678830d66be28ac373ab391267e9667e
nmosier <nmosier@middlebury.edu>
2019-10-26 19:21:15 +0000

    added handler files

---
+++ src/lib/middfs-handle.c
@@ -0,0 +1,10 @@
+/* middfs-handle.c -- middfs packet handler module 
+ * Nicholas Mosier & Tommaso Monaco 2019 */
+
+#include "middfs-pkt.h"
+#include "middfs-handle.h"
+
+
+int handle_packet(struct middfs_packet *pkt) {
+  return -1; /* STUB */
+}
---
+++ src/lib/middfs-handle.h
@@ -0,0 +1,9 @@
+/* middfs-handle.h -- middfs packet handler module 
+ * Nicholas Mosier & Tommaso Monaco 2019 */
+
+#ifndef __MIDDFS_HANDLE_H
+#define __MIDDFS_HANDLE_H
+
+
+
+#endif
