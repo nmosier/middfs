c53f77a221ebf0cb99bd92903c019b500df40473
nmosier <nmosier@middlebury.edu>
2019-11-15 02:05:47 +0000

    extricated server-specific handlers from library

---
+++ src/client/middfs-client-lstn.c
@@ -0,0 +1,7 @@
+/* middfs-client-lstn.c -- middfs client listener
+ * Nicholas Mosier & Tommaso Monaco 2019
+ */
+
+#include "middfs-client-lstn.h"
+
+
---
+++ src/client/middfs-client-lstn.h
@@ -0,0 +1,10 @@
+/* middfs-client-lstn.h -- middfs client listener
+ * Nicholas Mosier & Tommaso Monaco 2019
+ */
+
+#ifndef __MIDDFS_CLIENT_LSTN_H
+#define __MIDDFS_CLIENT_LSTN_H
+
+
+
+#endif
