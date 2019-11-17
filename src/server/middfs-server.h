bae31a3b5a736c7061ba1157cbd921205e2cca03
Nicholas Mosier <nh.mosier@gmail.com>
2019-11-17 15:56:26 -0500

    initial commit

---
+++ src/server/middfs-server.h
@@ -0,0 +1,10 @@
+/* middfs-server.h -- middfs server main
+ * Nicholas Mosier & Tommaso Monaco 2019
+ */
+
+#ifndef __MIDDFS_SERVER_H
+#define __MIDDFS_SERVER_H
+
+extern struct clients clients;
+
+#endif
