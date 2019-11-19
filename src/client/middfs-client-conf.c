ebfb84f2300789b9f3741bd48a4d2ecedd330c06
Nicholas Mosier <nh.mosier@gmail.com>
2019-11-19 13:00:52 -0500

    initialcommit

---
+++ src/client/middfs-client-conf.c
@@ -0,0 +1,10 @@
+/* middfs-client-conf.c -- middfs client configuration functions
+ * Nicholas Mosier & Tommaso Monaco 2019
+ */
+
+#include <stdlib.h>
+
+#include "lib/middfs-util.h"
+
+#include "client/middfs-client-conf.h"
+
---
+++ src/client/middfs-client-conf.h
@@ -0,0 +1,14 @@
+/* middfs-client-conf.h -- middfs client configuration functions 
+ * Nicholas Mosier & Tommaso Monaco 2019
+ */
+
+#ifndef __MIDDFS_CLIENT_CONF_H
+#define __MIDDFS_CLIENT_CONF_H
+
+#define MIDDFS_CONF_USERNAME "username"
+#define MIDDFS_CONF_LOCALPORT "localport"
+#define MIDDFS_CONF_REMOTEPORT "remoteport"
+#define MIDDFS_CONF_MOUNTPOINT "mountpoint"
+#define MIDDFS_CONF_HOMEPATH "homepath"
+
+#endif
